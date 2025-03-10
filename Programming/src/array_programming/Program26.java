993fb88aa9faf17a815838ee2d6701ca684a0526
surya <suryap.7493@gmail.com>
2025-03-10 10:21:26 +0530

    Frequency in range

---
+++ Programming/src/array_programming/Program26.java
@@ -0,0 +1,22 @@
+package array_programming;
+
+
+public class Program26 {
+
+	public static void main(String[] args) {
+		int[] a = {12,23,23,12,45,12,76,20,12};
+		getFrequency(a);
+	}
+	
+	public static void getFrequency(int[] a) {
+		int[] x = new int[1001];
+		for(int i = 0; i < a.length; i++) {
+			x[a[i]]++;
+		}
+		for(int i = 0; i< x.length; i++) {
+			if(x[i]> 0) {
+				System.out.println(i + " is "+ x[i]+ " times");
+			}
+		}
+	}
+}
