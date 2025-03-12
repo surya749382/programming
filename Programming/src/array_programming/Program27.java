e6be4238141b68c48a29bfcd0e9f5d2488d2a10a
surya <suryap.7493@gmail.com>
2025-03-12 21:18:14 +0530

    frequency count of elements

---
+++ Programming/src/array_programming/Program27.java
@@ -0,0 +1,26 @@
+package array_programming;
+
+public class Program27 {
+	public static void main(String[] args) {
+		int[] a = {12,23,23,12,45,12,76,20,12};
+		int start = 0;
+		int end  = a.length-1;
+		
+		for(int i = start; i <= end; i++ ) {
+			int count = 1;
+			for(int j = i+1; j <= end ; j++) {
+				if(a[i] == a[j] ) {
+					count++;
+					int temp = a[j];
+					a[j] = a[end];
+					a[end]= temp;
+					j--;		
+					end--; 
+					
+				}
+			}
+			System.out.println("The Frequency of "+ a[i] + " is "+ count);
+		}
+		
+	}
+}
